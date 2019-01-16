be21f2e9ffe0bb719960c8aed0a4168f0dbf5d7e
RajeswariBommisetti <rajeshwari.iiit@gmail.com>
2019-01-16 16:04:11 +0530

    * Added routing to app.js for charts and upload media.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Chart from './components/Chart';
+import Media from './components/Media';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {/* Creating routes to navigate easily using react router */}
+        <BrowserRouter>
+          <div className="App">
+              <Switch>
+              <Route path="/charts" component={Chart} />
+              <Route path="/uploadImages" component={Media} />
+              <Route path="/" component={Chart}/>
+              </Switch>
+          </div>
+          </BrowserRouter>
       </div>
     );
   }
